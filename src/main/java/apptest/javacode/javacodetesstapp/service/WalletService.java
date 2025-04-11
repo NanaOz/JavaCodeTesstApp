51987ca14131dc4d5b205c4c42d27f32dd34a2ee
Alina <alinavik93@gmail.com>
2025-04-11 14:52:20 +0500

    add docs in service

--- src/main/java/apptest/javacode/javacodetesstapp/service/WalletService.java
+++ src/main/java/apptest/javacode/javacodetesstapp/service/WalletService.java
@@ -17,6 +17,13 @@ public class WalletService {
         this.walletRepository = walletRepository;
     }
 
+    /**
+     * Обновляет баланс указанного кошелька в зависимости от типа операции (DEPOSIT или WITHDRAW).
+     *
+     * @param walletId      идентификатор кошелька, который необходимо обновить
+     * @param operationType тип операции (DEPOSIT или WITHDRAW)
+     * @param amount        сумма, которую необходимо внести или вывести
+     */
     @Transactional
     public void updateBalance(UUID walletId, String operationType, BigDecimal amount) {
         Wallet wallet = walletRepository.findById(walletId).orElseThrow(() -> new EntityNotFoundException("Wallet not found"));
@@ -34,15 +41,26 @@ public class WalletService {
         walletRepository.save(wallet);
     }
 
+    /**
+     * Получает текущий баланс указанного кошелька.
+     *
+     * @param walletId идентификатор кошелька, для которого необходимо получить баланс
+     * @return текущий баланс кошелька
+     */
     public BigDecimal getBalance(UUID walletId) {
         Wallet wallet = walletRepository.findById(walletId).orElseThrow(() -> new EntityNotFoundException("Wallet not found"));
         return wallet.getBalance();
     }
 
+    /**
+     * Создает новый кошелек с заданным начальным балансом.
+     *
+     * @param initialBalance начальный баланс для нового кошелька
+     * @return созданный кошелек
+     */
     public Wallet createWallet(BigDecimal initialBalance) {
         Wallet wallet = new Wallet();
         wallet.setBalance(initialBalance);
         return walletRepository.save(wallet);
     }
-
 }
